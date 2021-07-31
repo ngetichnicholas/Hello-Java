a6de7196197754a92811a583769b8a3433e18571
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-31 22:21:24 +0300

    Music

--- Meals.java
+++ Meals.java
@@ -1,26 +1,17 @@
-
-    import java.io.Console;
+import java.io.Console;
 
     public class Meals {
       public static void main(String[] args) {
-        Console myConsole = System.console();
         askWhatYouAteFor("breakfast");
-        String yourBreakfast = myConsole.readLine();
-        System.out.println("You had " + yourBreakfast + " for breakfast.");
-
         askWhatYouAteFor("lunch");
-        String yourLunch = myConsole.readLine();
-        System.out.println("You had " + yourLunch + " for lunch.");
-
         askWhatYouAteFor("dinner");
-        String yourDinner = myConsole.readLine();
-        System.out.println("You had " + yourDinner + " for dinner.");
       }
 
       public static void askWhatYouAteFor(String meal) {
+        Console myConsole = System.console();
         System.out.println("What did you eat for " + meal + "?");
+        String yourMeal = myConsole.readLine();
+        System.out.println("You had " + yourMeal + " for " + meal + ".");
       }
-
-
     }
   
--- Music.java
+++ Music.java
@@ -1,19 +1,18 @@
 import java.io.Console;
 public class Music {
   public static void main(String[] args) {
-    Console myConsole = System.console();
     myMusic("classical");
-    String classical = myConsole.readLine();
-    System.out.println("You truly like " + classical + " song in classical music");
+
 
     myMusic("pops");
-    String pops = myConsole.readLine();
-    System.out.println("You truly like " + pops + " song in pops in music");
 
   }
-
   public static void myMusic(String song) {
+    Console myConsole = System.console();
+
     System.out.println("Give your best song in " + song );
+    String yourSong = myConsole.readLine();
+    System.out.println("You truly like " + yourSong + " song in " + song + " music");
   }
   
 }
