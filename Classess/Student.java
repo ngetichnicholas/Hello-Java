e1809ebff5b29c069d2556a76f4cde9ceb2e0a1f
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-01 00:24:33 +0300

    Java Classess

---
+++ Classess/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+  String firstName;
+  String lastName;
+  int age;
+  boolean graguate;
+
+  public static void main(String[] args) {
+    Student myStudent = new Student();
+    myStudent.firstName = "Nick";
+    myStudent.lastName = "Ngetich";
+    myStudent.age = 25;
+    myStudent.graguate = false;
+  
+    System.out.println(myStudent.firstName);
+  
+  
+  }
+}
+
